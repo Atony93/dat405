897dc8fa577fc2adf9f644189e218ac23dba8d0a
Atony <atonyp93@gmail.com>
2018-10-23 16:32:53 +0100

    test 6

--- dat405-master/01_Project_Setup - Copy/sketch.js
+++ dat405-master/01_Project_Setup - Copy/sketch.js
@@ -7,10 +7,17 @@ function setup() {
 	// Set colours
 	fill(204, 101, 192,127);
 	stroke (127, 63, 120);
-	//rectangle
+	//a rectangle
 	rect(40, 120, 120, 40);
-	//anellispe
+	//an ellispe
 	ellipse(240, 240, 80, 80);
-	//triangle
+	//a triangle
 	triangle(300, 100, 320, 100, 310, 80);
+	// A design for a simple flower
+  translate(580, 200);
+  noStroke();
+  for (var i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
 }
