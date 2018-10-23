74afbf86473f4c401e93272b59693b68e25b45a7
Atony <atonyp93@gmail.com>
2018-10-23 16:19:59 +0100

    test5

---
+++ dat405-master/01_Project_Setup - Copy/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+// create the canvas
+	createCanvas (720, 400);
+	background(200)
+
+	// Set colours
+	fill(204, 101, 192,127);
+	stroke (127, 63, 120);
+	//rectangle
+	rect(40, 120, 120, 40);
+	//anellispe
+	ellipse(240, 240, 80, 80);
+	//triangle
+	triangle(300, 100, 320, 100, 310, 80);
+}
