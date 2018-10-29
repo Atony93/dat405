c582d39054a3a9171cf88c0fd7d0eec40daaef48
Atony <atonyp93@gmail.com>
2018-10-29 12:31:39 +0000

    mario

---
+++ dat405-master/Pixle/sketch.js
@@ -0,0 +1,66 @@
+//Initialization function
+function setup () {
+	createCanvas (130,180);
+	background (255,255,255,);
+	frameRate (60);
+}
+
+	function draw(){
+		// removing stroke
+		noStroke();
+
+		//line1
+		rect (30,20,10,10)
+		// line 2
+		fill (255,0,0)
+		rect	(30,20,60,10)
+		//line 3
+		rect (20,30,120,10)
+		//line4
+		fill (139,51,0)
+		rect (20,40,30,10)
+		fill (255,178,127)
+		rect (50,40,30,10)
+		fill (0,0,0)
+		rect (80,40,10,10)
+		fill (255,178,127)
+		rect (90,40,10,10)
+		//line5
+		fill (139,51,0)
+	 	rect (10,50,10,10)
+		fill (255,178,127)
+		rect (20,50,10,10)
+		fill (139,51,0)
+		rect (30,50,10,10)
+		fill (255,178,127)
+		rect (40,50,40,10)
+		fill (0,0,0)
+		rect (80,50,10,10)
+		fill (255,178,127)
+		rect (90,50,30,10)
+		//line6
+		fill (139,51,0)
+		rect (10,60,10,10)
+		fill (255,178,127)
+		rect (20,60,10,10)
+		fill (139,51,0)
+		rect (30,60,20,10)
+		fill (255,178,127)
+		rect (50,60,40,10)
+		fill (0,0,0)
+		rect (90,60,10,10)
+		fill (255,178,127)
+		rect (100,60,30,10)
+		//line7
+		fill (139,51,0)
+		rect (10,70,20,10)
+		fill (255,178,127)
+		rect (30,70,50,10)
+		fill (0,0,0)
+		rect (80,70,40,10)
+		//line7
+		fill (255,178,127)
+		rect (30,80,80,10)
+
+
+}
--- dat405-master/shapes/sketch.js
+++ dat405-master/shapes/sketch.js
@@ -1,42 +1,19 @@
-//https://github.com/stavrosdidakis/DAT-GAD-405_2017
-//https://p5js.org
 
-//DAT405 / GAD405
-//01_ProjectSetup
-//In this example we see how to create a canvas, set and display
-//the frame rate of the rendering system, and use text to
-//display the frame count and rate on screen
-
-//Initialization function
 function setup() {
-  //Set the size of rendering window - pixels
-  createCanvas(400, 400);
+// create the canvas
+	createCanvas (720, 400);
+	background(200)
 
-  //Set up the frame rate (frames per second)
-  //Default is 60 fps
-  frameRate(1);
+	// Set colours
+	fill(204, 101, 192,127);
+	stroke (127, 63, 120);
 
-  //Preferences for the text (size, and alignment)
-  textSize(20);
-  textAlign(CENTER);
-
-  fill(255, 0, 0);
-  rect(width/2, height/2, 50, 50);
-}
-
-//Rendering function
-function draw() {
-  //Erase all canvas. Set the color to dark grey
-  background(60);
-
-  //Set the color of the text
-  fill(255);
-  //Set the text message as the frame count
-  //Position text in the middle of the screen
-  text("Frame Count:  " + frameCount, width/2, height/2-20);
-  //Set the text message as the frame rate (round it to an integer)
-  //Position text in the middle of the screen
-  text("Frame Rate:  " + Math.round(frameRate()), width/2, height/2+20);
-
-  //noLoop();
+	//an ellispe 1
+	ellipse(340, 240, 80, 80);
+  //an ellispe 2
+	ellipse(340, 240, 80, 80);
+  //an ellispe 3
+  ellipse(340, 240, 80, 80);
+  //an ellispe 4
+  ellipse(340, 240, 80, 80);
 }
