e12a3023e11b67f5876c39f6641b042238e6f397
Atony <atonyp93@gmail.com>
2018-10-30 17:32:24 +0000

    done

--- dat405-master/loops/sketch.js
+++ dat405-master/loops/sketch.js
@@ -1,17 +1,26 @@
+  let size2 = 30;
   let size = 50;
   function setup () {
+      frameRate(2);
       createCanvas (500,500);
-      noLoop();
 
   }
   function draw() {
-      fill (215, 200,170);
-      for (var y = 0; y < 10; y+=3.4)
+        fill (255);
+      for (var y = 0; y < 10; y++)
       {
         for (let x = 0; x < 10; x++) {
-        fill (128,0,128)
         rect (size*x, size*y, size, size);
-        console.log("X: " + x + ", Y: " + y)
+
+        }
+      }
+      for (var y2 = 0; y2 < 10; y2++)
+      {
+
+        for (let x2 = 0; x2 < 10; x2++) {
+        fill (random(255), random(255), random(255))
+        rect (size*x2 +10, size*y2 + 10, size2, size2);
+
         }
       }
   }
