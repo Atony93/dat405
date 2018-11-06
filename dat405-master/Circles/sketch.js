0ed7e60d87b4ac0cd54ac2c5a2f902960a5b3bda
Atony <atonyp93@gmail.com>
2018-11-06 16:34:52 +0000

    whey

---
+++ dat405-master/Circles/sketch.js
@@ -0,0 +1,11 @@
+//let size= random(500);
+function setup() {
+  createCanvas(1000,1000);
+  frameRate(15);
+}
+
+function draw() {
+  fill (random(255),random(255),random(255),random(50));
+  noStroke()
+  ellipse (random(1000),random(1000),random(400));
+}
--- dat405-master/S4-01_Exercise01-Loops_And_Random_Color_(Solved)/sketch.js
+++ dat405-master/S4-01_Exercise01-Loops_And_Random_Color_(Solved)/sketch.js
@@ -1,25 +1,11 @@
-//DAT405 / GAD405
-//04_Exercise01 - Loops and Random Color
-
-let size = 50;
-
+let size= random(500);
 function setup() {
-  createCanvas(500, 500);
-  frameRate(3); //Drawing speed 3 fps
+  createCanvas (1000,1000);
+  framerate(15);
+
 }
 
-function draw() {
-  //Run the nested loop for x, and y
-  for (let y = 0; y < 10; y++) {
-    for (let x = 0; x < 10; x++) {
-      //Draw the background shapes
-      fill(215, 200, 170);
-      rect(size * x, size * y, size, size);
-
-      //Draw the foreground shapes with a random RGB color
-      fill(random(255), random(255), random(255));
-      //Create a slight offeset for the x and y, to appear in the middle
-      rect(12.5+size * x, 12.5+size * y, size/2, size/2);
-    }
-  }
+function draw(){
+  fill (random(255),random(255),random(255));
+  ellipse (random(100),random(100),random(200));
 }
