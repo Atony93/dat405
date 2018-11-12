88b63b83910f3a39de911142d889de7c1244c7ce
Atony <atonyp93@gmail.com>
2018-11-12 11:00:01 +0000

    Still not working

--- dat405-master/HW/Shapes 2/00_Template/sketch.js
+++ dat405-master/HW/Shapes 2/00_Template/sketch.js
@@ -2,50 +2,51 @@
   createCanvas(1000,1000);
   frameRate(15);
 }
-function draw(){
 
-  if (keyIsDown(A)){
+function draw(){
+  if (key == 'a'){
     shape1();
     }
-  if (keyIsDown(B)){
+  if (key == 's'){
     shape2();
     }
-  if (keyIsDown(C)){
+  if (key == 'd'){
     shape3();
   }
+}
 
 function shape1(){
-  let x = (random(500));
-  let y = (random(500));
+  let x = random(500);
+  let y = random(500);
   let size = 100;
 
-  fill(255,255,255);
-  ellipse (x, y, size);
-  fill (255,155,200);
-  ellipse (x, y, size - 10);
-  fill (255,255,255);
-  ellipse (x, y, size - 20);
+  /*fill(0, 255, 255);
+  ellipse (x, y, size, size);
+  fill (255, 155, 200);
+  ellipse (x, y, size - 10, size - 10);
+  fill (0, 255, 255);
+  ellipse (x, y, size - 20, size - 20);
   fill()
-  ellipse (x, y, size - 35);
-  stroke(0,0,0);
+  ellipse (x, y, size - 40, size - 40);
+  stroke(0);
   strokeWeight(5);
-  ellipse (x, y, size - 55);
+  ellipse (x, y, size - 60, size - 60);
   stroke(0);
   strokeWeight(1);
-  fill(0,255,255);
-  ellipse (x, y, size - 75);
+  fill(0, 255, 255);
+  ellipse (x, y, size - 80, size - 80);*/
 
 }
 
 function shape2(){
-  let x = (random(500));
-  let y = (random(500));
+  let x = random(500);
+  let y = random(500);
   let size = 60;
 
   fill(120,30,230);
-  ellipse (xCoor, yCoor, 50);
-  fill(75,75,200);
-  triangle(x + 40, y +60, x, y);
+  ellipse (x, y, 50, 50);
+  //fill(75,75,200);
+  //triangle(x + 40, y +60, x, y);
 }
 
 function shape3(){
