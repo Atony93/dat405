1917392fe5f7a3b86d9d05e25b845c7d876fcfba
Atony <atonyp93@gmail.com>
2018-11-12 11:48:18 +0000

    working

--- dat405-master/HW/Shapes 2/00_Template/sketch.js
+++ dat405-master/HW/Shapes 2/00_Template/sketch.js
@@ -1,10 +1,17 @@
+var s = 1 ;
  function setup(){
   createCanvas(1000,1000);
   frameRate(15);
 }
 
 function draw(){
-  if (key == 'a'){
+
+if (s == 1) shape1 ();
+else if (s==2) shape2();
+else if (s==3) shape3();
+
+
+/*  if (key == 'a'){
     shape1();
     }
   if (key == 's'){
@@ -12,15 +19,22 @@ function draw(){
     }
   if (key == 'd'){
     shape3();
-  }
+  }*/
+
+}
+function keyPressed (){
+  if (key ==1 ) s = 1;
+  if (key ==2 ) s = 2;
+  if (key ==3 ) s = 3;
 }
 
+// circle
 function shape1(){
   let x = random(500);
   let y = random(500);
   let size = 50;
 
-  fill(0, 0, 0);
+  fill(0, 0, 0, 50);
   ellipse (x, y, size, size);
   /*
   fill (255, 155, 200);
@@ -38,7 +52,7 @@ function shape1(){
   ellipse (x, y, size - 80, size - 80);*/
 
 }
-
+// triangle
 function shape2(){
   let x = random(500);
   let y = random(500);
@@ -46,19 +60,19 @@ function shape2(){
 
 /*  fill(120,30,230);
   ellipse (x, y, 50, 50);*/
-  fill(75,75,200);
-  triangle(x + 40, y +60, x, y, x - 40, y+60 );
+  fill(45, 242, 255, 50);
+  triangle(x +40, y +60, x, y, x -40, y +60 );
 }
-
+// square
 function shape3(){
   let x = (random(500));
   let y = (random(500));
   let size = 50;
 
-  fill(120,35,230);
+  fill(255, 14, 35, 50);
   rect(x, y, size, size);
-  fill(255,80,90);
+  fill(76, 255, 51,50);
   rect(x+5, y+5, size-10, size-10);
-  fill(255,255,255);
+  fill(255, 255, 255);
   rect(x+10, y+10, size-20, size-20);
 }
