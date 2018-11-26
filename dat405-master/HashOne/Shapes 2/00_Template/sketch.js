c1f9b76ab77441091098ecc5ad5c8e30226c793e
Atony <atonyp93@gmail.com>
2018-11-26 20:27:40 +0000

    stressed out

--- dat405-master/HashOne/Shapes 2/00_Template/sketch.js
+++ dat405-master/HashOne/Shapes 2/00_Template/sketch.js
@@ -1,48 +1,68 @@
-  var xc = [];
-  var yc = [];
-  var rc = [];
-  var re, g, b;
-  var maxCount = 1000; // total amount of ellipse's created
-  var currentCount = 10;
-  function setup() {
-    createCanvas(594,841); // canvas size
-    strokeWeight(0.25); // ellipse,s outline
-    frameRate(1); // speed of ellipses created per second
+var xc = [];
+var yc = [];
+var rc = [];
+//var bg;
+var re, g, b;
+var maxCount = 1000; // total amount of ellipse's created
+var currentCount = 10;
 
-  xc[0] = mouseX;  // first ellipse location
-  yc[0] = mouseY;
+function setup() {
+  createCanvas(594,841); // canvas size
+  //bg = loadImage("snowflake.jpg");
+  //background(bg);
+  strokeWeight(0.25); // ellipse,s outline
+  frameRate(1); // speed of ellipses created per second
+
+  xc[0] = width/2;  // first ellipse location
+  yc[0] = height/2;
   rc[0] = 5;
 }
-  function draw() { // creates a random set of parameters
 
+function draw() { //12 creates a random set of parameters
   var closestIndex = 0;
   var closestDist = Number.MAX_VALUE;
   var newR = random(1, 7);
   var newX = random(newR, width - newR);
   var newY = random(newR, height - newR);
+
+console.log(currentCount)
+
   for (var i = 0; i < currentCount; i++) {
-  var newDist = dist(newX, newY, xc[i], yc[i]);
-  if (newDist < closestDist) {
+    var newDist = dist(newX, newY, xc[i], yc[i]);
+    if (newDist < closestDist) {
       closestDist = newDist;
       closestIndex = i;
     }
+    fill(re, g, b );
+    ellipse(xc[i], yc[i], rc[i] * 2, rc[i] * 2);
   }
+
   var angle = atan2(newY - yc[closestIndex], newX - xc[closestIndex]);
   xc[currentCount] = xc[closestIndex] + cos(angle) * (rc[closestIndex] + newR);
   yc[currentCount] = yc[closestIndex] + sin(angle) * (rc[closestIndex] + newR);
   rc[currentCount] = newR;
   currentCount++;
-  // drawing ellipse's
-  for (var i = 0; i < currentCount; i++) {
-    fill(re, g, b );
-    ellipse(xc[i], yc[i], rc[i] * 2, rc[i] * 2);
-  }
-  }
-  function keyPressed() { // function keys
-    if (key=='1') frameRate (60); // increases speed of ellipse's
-    if (key=='2') frameRate (1); // decreases speed of ellipse's
-    if (key=='3') re=255; g=0; b=0; // changes colour - pink
-    if (key=='4') re=0; g=255; b=255; // changes colour - blue
-    if (key=='5') re=150; g=50; b=255; // changes colour - violet
+}
 
+function mousePressed(){
+  //background(255);
+  for (var i = 0; i < currentCount; i++) {
+    xc = [];  // first ellipse location
+    yc = [];
+    rc = [];
+
+    xc[0] = mouseX;  // first ellipse location
+    yc[0] = mouseY;
+    rc[0] = 5;
   }
+    //rc[0] = 5;
+}
+
+function keyPressed() { // function keys
+  if (key=='1') frameRate (60); // increases speed of ellipse's
+  if (key=='2') frameRate (1); // decreases speed of ellipse's
+  if (key=='3') re=211; g=211; b=211, 50; // changes colour - 
+  if (key=='4') re=0; g=255; b=255, 50; // changes colour - blue
+  if (key=='5') re=150; g=50; b=255, 50; // changes colour - teal
+  if (key=='6') re=255; g=255; b=255, 50; // changes colour - white
+}
---
+++ dat405-master/Mapping/sketch.js
@@ -0,0 +1,36 @@
+var prevS = 0;
+function setup(){
+  createCanvas(400, 400);
+
+}
+
+function draw(){
+  var size = 50;
+  background(0);
+  var xLine = map(mouseX, 0, height, 0, 255);
+  var yLine = map(mouseY, 0, width, 0, 255);
+  var xRest = map(mouseY, 0, width, 255, 0);
+
+  for (var y = 0; y < 400; y+=size) {
+    for (var x = 0; x < 400; x+=size) {
+      if (mouseX > x && mouseY > y && mouseX < x + size && mouseY < y + size) {
+        fill(255, 0, 0);
+        rect(x, y, size, size);
+      }
+      else if (mouseX > x && mouseX < x + size) {
+        fill(0, 0, 255, xLine);
+        rect(x, y, size, size);
+      }
+      else if (mouseY > y && mouseY < y + size) {
+        fill(0, 0, 255, yLine);
+        rect(x, y, size, size);
+      }
+      else{
+        fill(255,165,0, xRest);
+        rect(x, y, size, size);
+      }
+    }
+  }
+
+
+}
