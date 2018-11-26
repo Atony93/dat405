4eb19dd2a7198ea3946e8b05bc7dffebc3859953
Atony <atonyp93@gmail.com>
2018-11-26 09:40:58 +0000

    finsihed

--- dat405-master/HashOne/Shapes 2/00_Template/sketch.js
+++ dat405-master/HashOne/Shapes 2/00_Template/sketch.js
@@ -1,67 +1,47 @@
-
-
-  var x = [];
-  var y = [];
-  var r = [];
+  var xc = [];
+  var yc = [];
+  var rc = [];
   var re, g, b;
-  var maxCount = 500;
+  var maxCount = 1000; // total amount of ellipse's created
   var currentCount = 10;
   function setup() {
-    createCanvas(windowWidth, windowHeight);
-    strokeWeight(0.5);
-    frameRate(15);
+    createCanvas(594,841); // canvas size
+    strokeWeight(0.25); // ellipse,s outline
+    frameRate(1); // speed of ellipses created per second
 
-
-  // first ellipse location
-  x[0] = mouseX;
-  y[0] = mouseY;
-  r[0] = 5;
+  xc[0] = mouseX;  // first ellipse location
+  yc[0] = mouseY;
+  rc[0] = 5;
 }
+  function draw() { // creates a random set of parameters
 
-  function draw() {
-  //clear();
-  //if (keyIsPressed) {
-    //frameRate(200);
-  //}
-
-  // creates a random set of parameters
+  var closestIndex = 0;
+  var closestDist = Number.MAX_VALUE;
   var newR = random(1, 7);
   var newX = random(newR, width - newR);
   var newY = random(newR, height - newR);
-
-  var closestDist = Number.MAX_VALUE;
-  var closestIndex = 0;
-
-  // closest circle
   for (var i = 0; i < currentCount; i++) {
-  var newDist = dist(newX, newY, x[i], y[i]);
+  var newDist = dist(newX, newY, xc[i], yc[i]);
   if (newDist < closestDist) {
       closestDist = newDist;
       closestIndex = i;
     }
   }
-
-  var angle = atan2(newY - y[closestIndex], newX - x[closestIndex]);
-
-  x[currentCount] = x[closestIndex] + cos(angle) * (r[closestIndex] + newR);
-  y[currentCount] = y[closestIndex] + sin(angle) * (r[closestIndex] + newR);
-  r[currentCount] = newR;
+  var angle = atan2(newY - yc[closestIndex], newX - xc[closestIndex]);
+  xc[currentCount] = xc[closestIndex] + cos(angle) * (rc[closestIndex] + newR);
+  yc[currentCount] = yc[closestIndex] + sin(angle) * (rc[closestIndex] + newR);
+  rc[currentCount] = newR;
   currentCount++;
-
-  // createing ellipse's
+  // drawing ellipse's
   for (var i = 0; i < currentCount; i++) {
     fill(re, g, b );
-    ellipse(x[i], y[i], r[i] * 2, r[i] * 2);
-
+    ellipse(xc[i], yc[i], rc[i] * 2, rc[i] * 2);
   }
-
-  //if (currentCount >= maxCount) noLoop();
   }
-  function keyPressed() {
-    if (key=='1') clear();
-    if (key=='2') frameRate (60);
-    if (key=='3') re=255; g=0; b=0;
-    if (key=='4') re=0; g=255; b=255;
-    if (key=='5') re=150; g=50; b=255;
-
+  function keyPressed() { // function keys
+    if (key=='1') frameRate (60); // increases speed of ellipse's
+    if (key=='2') frameRate (5); // decreases speed of ellipse's
+    if (key=='3') re=255; g=0; b=0; // changes colour - pink
+    if (key=='4') re=0; g=255; b=255; // changes colour - blue
+    if (key=='5') re=150; g=50; b=255; // changes colour - violet
   }
