0ef0d0b484711ac44de55f4c833553d119d51049
Atony <atonyp93@gmail.com>
2018-11-26 11:28:03 +0000

    done

--- dat405-master/Shapes 2/00_Template/sketch.js
+++ dat405-master/Shapes 2/00_Template/sketch.js
@@ -1,69 +1,54 @@
-//https://github.com/stavrosdidakis/DAT-GAD-405_2017
-//https://p5js.org
-
-//DAT405 / GAD405
-//00_Template
-//The purpose of this sketch is to provide a basic template
-//for P5.js projects
-
 //Initialization function
 function setup() {
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Initialization: OK")
-  createCanvas(500,300);
-  //Set a background color
-  background(160);
+  createCanvas(500, 300);
+  background(60);
 }
+  function draw(){
 
-//Rendering function
-function draw() {
+    //setting stroke width
+    strokeWeight(5);
 
-// stroke width
-strokeWright (5);
+    //line top left to bottom right
+    stroke(0, 255, 0);
+    line(25, 25, 475, 275);
 
-//line from top left to bottom right
-stroke(0,255,0);
-line(25,25,475,275);
+    //line top right to bottom left
+    stroke(255, 255, 0);
+    line(475, 25, 25, 275);
 
-//line from top right to bottom left
-stroke(255,255,0);
-line(475,25,475,275);
+    //line top left to top right
+    stroke(255, 0, 255);
+    line(25, 25, 475, 25);
 
-//line from top left to top right
-stroke(255,0,255);
-line(25,25,475,25);
+    //line top left bottom left
+    stroke(0, 255, 255);
+    line(25, 25, 25, 275);
 
-//line from  top left to bottom left
-stroke(0,255,255);
-line(25,25,25,275);
+    //line top right to bottom right
+    stroke(255, 160, 0);
+    line(475, 25, 475, 275);
 
-// line from top right to bottom right
-stroke(255,160,0);
-line(475,25,475,275);
+    //line bottom left to bottom right
+    stroke(150, 255, 0);
+    line(25, 275, 475, 275);
 
-//live from bottom left to bottom right
-stroke(150,255,0);
-line(25,275,475,275);
+    //removing stroke from circles
+    noStroke();
 
-noStoke();
+    //top left
+    fill(0, 0, 255, 1000);
+    ellipse(25, 25, 50, 50);
 
-//top left
-fill(0,255,1000);
-ellipse(25,25,50,50);
+    //top right
+    fill(255, 0, 0, 1000);
+    ellipse(475, 25, 50, 50);
 
-//top right
-]fill(255,0,0,1000);
-ellipse(475,25,50,50);
+    //bottom right
+    fill(255, 255, 0, 1000);
+    ellipse(475, 275, 50, 50);
 
-//bottom right
-fill(255,255,0,1000);
-ellipse(475,275,50,50);
+    //bottom left
+    fill(0, 255, 0, 1000);
+    ellipse(25, 275, 50, 50);
 
-//bottom left
-fill(0,255,0,1000);
-ellipse(25,275,50,50);
-
-  console.log("Rendering...")
-  noLoop();
 }
